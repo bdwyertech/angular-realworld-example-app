2dc14023f5ac68056b4e23cb2442e87833e6974b
Brian Dwyer <bdwyer@IEEE.org>
2017-12-12 21:12:55 -0500

    Revert "Allow Angular CLI to dynamically source configuration from the build environment"
    This reverts commit 81222bc6e907f4d226fde71421960f1b6c980867.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: process.env.API_URL || 'https://conduit.productionready.io/api'
+  api_url: 'https://conduit.productionready.io/api'
 };
